f4a102fdeda4923dafec2df30606896fdd8182f5
eli <eli107377@weexdev.com>
2024-11-08 16:56:55 +0800

    feat: seo and theme

---
+++ app/posts/page.tsx
@@ -0,0 +1,14 @@
+export const generateMetadata = () => {
+    return { 
+      title: "博客列表",
+      description: "这是博客列表页面",
+      openGraph: {
+        title: '博客列表',
+        description: '这是博客列表页面'
+      }
+    }
+  }
+
+export default function Posts() {
+  return <div>Posts</div>;
+}
---
+++ data/siteMetadata.ts
@@ -0,0 +1,11 @@
+const siteMetadata = {
+  siteUrl: "https://miao.com",
+  title: "Miao的技术博客",
+  description: "Miao的技术博客，分享技术、个人成长等内容",
+  author: "Miao",
+  locale: "zh-CN",
+  socialBanner:
+    "https://cdna.artstation.com/p/assets/images/images/028/138/058/large/z-w-gu-bandageb5f.jpg?1593594749",
+};
+
+export default siteMetadata;
